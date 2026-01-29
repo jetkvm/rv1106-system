3fc18283507c2db89f62a1e110575a6d8a66a7d5
Adam Shiervani <adam.shiervani@gmail.com>
2026-01-29 10:12:47 +0100

    Better log msg

--- scripts/flash_system.sh
+++ scripts/flash_system.sh
@@ -52,7 +52,7 @@ if [ ! -f "$ota_tar" ]; then
 fi
 
 msg_info ">> Flashing system image to device..."
-msg_info "  Transferring update_ota.tar..."
+msg_info "  Transferring update_ota.tar to /userdata/jetkvm/update_system.tar......"
 sshdev "cat > /userdata/jetkvm/update_system.tar" < "$ota_tar"
 msg_ok "  OK: Transfer complete"
 
